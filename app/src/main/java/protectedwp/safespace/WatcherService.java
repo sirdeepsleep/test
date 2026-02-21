5d45aa7d774629e67a5ddd3db7372fdddaecd951
confidenseuide <confidenseuide@proton.me>
2026-02-22 01:36:59 +0500

    Update WatcherService.java

--- app/src/main/java/protectedwp/safespace/WatcherService.java
+++ app/src/main/java/protectedwp/safespace/WatcherService.java
@@ -8,6 +8,7 @@ import android.content.ServiceConnection;
 import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
+import android.provider.Settings;
 
 public class WatcherService extends Service {
     private MediaPlayer player;
@@ -16,32 +17,36 @@ public class WatcherService extends Service {
         @Override public void onServiceConnected(ComponentName name, IBinder service) {}
         @Override
         public void onServiceDisconnected(ComponentName name) {
-            bindToNeighbor(); // Воскрешаем соседа
+            bindToNeighbor();
         }
     };
 
     private void bindToNeighbor() {
+        // Первый сервис тянет ВТОРОЙ
         Intent intent = new Intent(this, WatcherService2.class);
         bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // Запуск непрерывной музыки
         if (player == null) {
-            player = MediaPlayer.create(this, R.raw.alarm_sound);
-            player.setLooping(true); // Бесконечный повтор
-            player.setVolume(1.0f, 1.0f); // Максимальная громкость
-            player.start();
+            player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+            if (player != null) {
+                player.setLooping(true);
+                player.setVolume(1.0f, 1.0f);
+                player.start();
+            }
         }
-
         bindToNeighbor();
         return new Binder();
     }
 
     @Override
     public void onDestroy() {
-        if (player != null) player.release();
+        if (player != null) {
+            player.stop();
+            player.release();
+        }
         super.onDestroy();
     }
 }
