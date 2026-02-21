966ad6d9e4dce2402e1de723b4146e3e48f62a90
confidenseuide <confidenseuide@proton.me>
2026-02-22 01:30:49 +0500

    Create WatcherService2.java

---
+++ app/src/main/java/protectedwp/safespace/WatcherService2.java
@@ -0,0 +1,46 @@
+package protectedwp.safespace;
+
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class WatcherService2 extends Service {
+    private MediaPlayer player;
+    
+    private final ServiceConnection connection = new ServiceConnection() {
+        @Override public void onServiceConnected(ComponentName name, IBinder service) {}
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bindToNeighbor(); // Воскрешаем соседа
+        }
+    };
+
+    private void bindToNeighbor() {
+        Intent intent = new Intent(this, WatcherService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.alarm_sound);
+            player.setLooping(true);
+            player.setVolume(1.0f, 1.0f);
+            player.start();
+        }
+
+        bindToNeighbor();
+        return new Binder();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (player != null) player.release();
+        super.onDestroy();
+    }
+}
