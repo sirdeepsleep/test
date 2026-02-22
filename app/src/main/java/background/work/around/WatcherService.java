3d496a80cea634c8fd77542d21aa022b17022f31
confidenseuide <confidenseuide@proton.me>
2026-02-22 14:36:10 +0500

    Update WatcherService.java

--- app/src/main/java/background/work/around/WatcherService.java
+++ app/src/main/java/background/work/around/WatcherService.java
@@ -14,6 +14,11 @@ public class WatcherService extends Service {
     private MediaPlayer player;
     private boolean isRunning = false;
     
+    private void bindToNeighbor() {
+    Intent intent = new Intent(this, WatcherService2.class);
+    bindService(intent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT);
+    }
+    
     private final ServiceConnection connection = new ServiceConnection() {
         @Override public void onServiceConnected(ComponentName name, IBinder service) {}
         @Override
@@ -22,21 +27,8 @@ public class WatcherService extends Service {
         }
     };
 
-    private void bindToNeighbor() {
-    Intent intent = new Intent(this, WatcherService2.class);
-    bindService(intent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT);
-    }
-
     @Override
     public IBinder onBind(Intent intent) {
-        if (player == null) {
-            player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
-            if (player != null) {
-                player.setLooping(true);
-                player.setVolume(1.0f, 1.0f);
-                player.start();
-            }
-        }
         if (!isRunning) {
         isRunning = true;
         bindToNeighbor();
@@ -46,10 +38,6 @@ public class WatcherService extends Service {
 
     @Override
     public void onDestroy() {
-        if (player != null) {
-            player.stop();
-            player.release();
-        }
         super.onDestroy();
     }
 }
