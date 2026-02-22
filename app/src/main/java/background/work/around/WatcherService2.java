dfaccab6d5ff5d6fd44f8157b64e2495e125ebae
confidenseuide <confidenseuide@proton.me>
2026-02-22 13:58:24 +0500

    Update WatcherService2.java

--- app/src/main/java/background/work/around/WatcherService2.java
+++ app/src/main/java/background/work/around/WatcherService2.java
@@ -24,7 +24,7 @@ public class WatcherService2 extends Service {
 
     private void bindToNeighbor() {
     Intent intent = new Intent(this, WatcherService2.class);
-    bindService(intent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT | Context.BIND_ABOVE_CLIENT);
+    bindService(intent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT);
     }
 
     @Override
